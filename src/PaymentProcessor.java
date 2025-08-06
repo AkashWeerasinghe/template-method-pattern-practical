642167b4aaa01ee50e9a4a7b5434bf4f49c752b1
Akash Weerasinghe <Akash Weerasinghe@AKASH-WEERASINGHE>
2025-08-06 15:17:59 +0530

    templatepattern

---
+++ src/PaymentProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Akash Weerasinghe
+ */
+public abstract class PaymentProcessor {
+    
+    protected double amount;
+    public static final double TAX_PERCENTAGE = 20;
+    
+    public PaymentProcessor(double amount){
+        this.amount = amount;
+    }
+    
+    protected void startTransaction(StringBuilder log){
+        log.append("Starting Transaction of amount: LKR ").append(String.format("%.2f", amount)).append("\n");
+    }
+    
+    protected void selectPaymentGateway(StringBuilder log){
+        log.append("Selecting payment gateway...\n");
+    }
+    
+    protected abstract void authenticate(StringBuilder log);
+    
+    protected abstract void pay(StringBuilder log);
+    
+    protected void calculateTax(StringBuilder log){
+        double tax = amount * (TAX_PERCENTAGE / 100);
+        double finalamount = amount + tax;
+        log.append("Calculating ")
+                .append(TAX_PERCENTAGE)
+                .append("% GST: LKR ")
+                .append(String.format("%.2f", tax))
+                .append("\n");
+        log.append("Final amount to be Paid: LKR")
+                .append(String.format("%.2f", finalamount))
+                .append("\n");
+    }
+    
+    protected void generateReceipt(StringBuilder log){
+        log.append("Generating Digital receipt...\n");
+    }
+}
